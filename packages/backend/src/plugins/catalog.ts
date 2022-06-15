6156b84d6edfbbd56de94fe59cf9425d5220b927
Haili Zhang <haili.zhang@outlook.com>
2022-06-15 15:28:47 +0800

    ✨ feat(catalog): introduce gitlab discovery

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,13 +1,28 @@
+import { Router } from 'express';
+
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
-import { Router } from 'express';
+import { GitlabDiscoveryEntityProvider } from '@backstage/plugin-catalog-backend-module-gitlab';
+
 import { PluginEnvironment } from '../types';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
+
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+
+  builder.addEntityProvider(
+    ...GitlabDiscoveryEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 30 },
+        timeout: { minutes: 3 },
+      }),
+    }),
+  );
+
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
